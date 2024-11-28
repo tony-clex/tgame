3caa906b4afcb574d88b7c34d2aeb63028d85d41
bimela tony <tonybimela@gmail.com>
2024-11-28 13:18:39 +0100

    addingchanges

---
+++ javascript/guessing.js
@@ -0,0 +1,15 @@
+const random = Math.floor(Math.random()*50+1);
+console.log(random)
+
+const check = document.getElementById("select")
+const enter = document.getElementById("sub")
+const gtreat = document.getElementById("result")
+
+enter.addEventListener('click', ()=>{
+    const guess =check.Value;
+    if (guess === random){
+        gtreat.textContent = "you got it!"
+    }else{
+        gtreat.textContent = "keep trying"
+    }
+})
